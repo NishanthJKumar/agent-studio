19cb9cc229c111a56b6cf631dd344e6ea86628e6
Nishanth Kumar <njkmr@meta.com>
2025-07-02 15:21:07 -0400

    efficiency optimization

--- agent_studio/agent/base_agent.py
+++ agent_studio/agent/base_agent.py
@@ -143,7 +143,10 @@ class BaseAgent:
             else:
                 code = code_clean
             logger.debug(f"Code to execute:\n{code}\n")
-            result = self.runtime(code)
+            if len(code) > 0:
+                self.runtime(code)
+            else:
+                result = {}
             # TODO: there might be other conditions to check for.
             if len(result.keys()) == 0 and exit_in_code:
                 done = True
