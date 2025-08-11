6eb106720e83a0b0da299d107e9cd10251de055b
NishanthJKumar <nishanth.kumar20@gmail.com>
2025-08-11 15:41:54 +0000

    minor change to analysis script

--- scripts/display_results/analyze_hint_exploration_data.py
+++ scripts/display_results/analyze_hint_exploration_data.py
@@ -9,12 +9,12 @@ folder_path = Path("exploration_for_finetuning_vscode")
 task_config_to_successes = {}
 for json_file in folder_path.glob("*.json"):
     filename = json_file.stem
-    task_name = filename.split("traj")[0]
+    task_name = filename.split("_traj")[0]
     data = read_json(json_file)
     outcome = data["outcome"]
     if task_config_to_successes.get(task_name) is None:
         task_config_to_successes[task_name] = []
-    task_config_to_successes[task_name].append(outcome == "success")
+    task_config_to_successes[task_name].append(outcome)
 
 for task_name, successes in task_config_to_successes.items():
     print(f"{task_name} has {sum(successes)} successes out of {len(successes)} trials")
