20e6ac517edb7000a857ec271c86d59ebf31266d
Nishanth Kumar <njkmr@meta.com>
2025-08-12 09:58:50 -0400

    hint exploration data analysis

--- scripts/display_results/analyze_hint_exploration_data.py
+++ scripts/display_results/analyze_hint_exploration_data.py
@@ -1,26 +1,32 @@
 """Analyzes a folder of JSON files corresponding to data collected during exploration."""
 
-
+import argparse
 from pathlib import Path
 from agent_studio.utils.json_utils import read_json
 
 
-folder_path = Path("exploration_for_finetuning_vscode")
-task_config_to_successes = {}
-for json_file in folder_path.glob("*.json"):
-    filename = json_file.stem
-    task_name = filename.split("_traj")[0]
-    data = read_json(json_file)
-    outcome = data["outcome"]
-    if task_config_to_successes.get(task_name) is None:
-        task_config_to_successes[task_name] = []
-    task_config_to_successes[task_name].append(outcome)
+def main():
+    parser = argparse.ArgumentParser(description="Analyze a folder of JSON files corresponding to data collected during exploration")
+    parser.add_argument("path", help="Path to the folder containing JSON files to analyze")
+    args = parser.parse_args()
+    folder_path = Path(args.path)
+    # Main results loop.
+    task_config_to_successes = {}
+    for json_file in folder_path.glob("*.json"):
+        filename = json_file.stem
+        task_name = filename.split("_traj")[0]
+        data = read_json(json_file)
+        outcome = data["outcome"]
+        if task_config_to_successes.get(task_name) is None:
+            task_config_to_successes[task_name] = []
+        task_config_to_successes[task_name].append(outcome)
 
-for task_name, successes in task_config_to_successes.items():
-    print(f"{task_name} has {sum(successes)} successes out of {len(successes)} trials")
+    # Results display.
+    for task_name, successes in task_config_to_successes.items():
+        print(f"{task_name} has {sum(successes)} successes out of {len(successes)} trials")
+    print(f"Total number of successes: {sum(sum(successes) for successes in task_config_to_successes.values())}")
+    print(f"Total number of trials: {sum(len(successes) for successes in task_config_to_successes.values())}")
 
-print(f"Total number of successes: {sum(sum(successes) for successes in task_config_to_successes.values())}")
-print(f"Total number of trials: {sum(len(successes) for successes in task_config_to_successes.values())}")
 
-
-
+if __name__ == "__main__":
+    main()
